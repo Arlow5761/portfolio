eff32bb4e62be781a6a63b8bde123db0e208821d
Arlow5761 <arlow5761@gmail.com>
2026-07-17 10:12:13 +0700

    chore: add page title to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,9 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+    <link rel="icon" href={favicon} />
+    <title>Arlow5761 — Portfolio</title>
+</svelte:head>
+
 {@render children()}
