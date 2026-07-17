e520c54acad09d7b7002bc7abb0e433b5ad037f5
Arlow5761 <arlow5761@gmail.com>
2026-07-17 13:25:37 +0700

    feat: ensure padding on image of error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -13,7 +13,7 @@
     <img
         src={errorimg}
         alt="Bocchi having a panic attack"
-        class="w-96 h-96 object-cover" />
+        class="min-w-0 w-96 max-w-[calc(100%-1rem)] aspect-square object-cover" />
     <h1 class="text-9xl font-display">{page.status}</h1>
     <p class="text-3xl">{page.error?.message}</p>
 </div>
